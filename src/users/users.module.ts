ecc30028ed2bbf570d20263fa23e5bdeab0811d7
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-09-15 20:43:34 +0530

    database connection successfull

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Users, UsersSchema } from 'src/model/user.schema';
+import { Users, UsersSchema } from 'src/model/users.schema';
 
 @Module({
   imports: [
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Users } from 'src/model/users.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(Users.name) private readonly usersModel: Model<Users>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const createdUser = new this.usersModel(createUserDto);
+    return await createdUser.save();
   }
 
   findAll() {
