261a980d2896a3ce75ad36d54f74b6668fefdd6b
Jagmeet Singh <89521118+ucantbe-jagmeet@users.noreply.github.com>
2024-08-26 21:53:32 +0530

    fix/removingCorsRrlSlash

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://user-one-phi.vercel.app/', 'http://localhost:3000'], // Add your allowed URLs here
+    origin: ['https://user-one-phi.vercel.app', 'http://localhost:3000'], // Add your allowed URLs here
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
     credentials: true, // Include credentials like cookies in CORS requests
   });
